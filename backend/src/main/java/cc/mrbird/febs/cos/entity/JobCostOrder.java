04a15c7d59f9d9f0f2a8f849138160b9aa28655e
Fankekeke <1258846421@qq.com>
2023-04-27 22:36:49 +0800

    fix -成本计算

--- backend/src/main/java/cc/mrbird/febs/cos/entity/JobCostOrder.java
+++ backend/src/main/java/cc/mrbird/febs/cos/entity/JobCostOrder.java
@@ -2,6 +2,9 @@ package cc.mrbird.febs.cos.entity;
 
 import java.time.LocalDateTime;
 import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -17,6 +20,12 @@ import lombok.experimental.Accessors;
 public class JobCostOrder implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
 
     /**
      * 创建人
@@ -36,7 +45,7 @@ public class JobCostOrder implements Serializable {
     /**
      * 创建时间
      */
-    private LocalDateTime createDate;
+    private String createDate;
 
     /**
      * 备注
---
+++ frontend/src/views/base/cost/CostAdd.vue
@@ -0,0 +1,154 @@
+<template>
+  <a-modal v-model="show" title="新增成本" @cancel="onClose" :width="800">
+    <template slot="footer">
+      <a-button key="back" @click="onClose">
+        取消
+      </a-button>
+      <a-button key="submit" type="primary" :loading="loading" @click="handleSubmit">
+        提交
+      </a-button>
+    </template>
+    <a-form :form="form" layout="vertical">
+      <a-row :gutter="20">
+        <a-col :span="12">
+          <a-form-item label='成本名称' v-bind="formItemLayout">
+            <a-input v-decorator="[
+            'orderName',
+            { rules: [{ required: true, message: '请输入名称!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label='上传人' v-bind="formItemLayout">
+            <a-input v-decorator="[
+            'createBy',
+            { rules: [{ required: true, message: '请输入上传人!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="24">
+          <a-form-item label='备注' v-bind="formItemLayout">
+            <a-textarea :rows="4" v-decorator="[
+            'remark',
+             { rules: [{ required: true, message: '请输入备注!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="24">
+          <a-form-item label='选择物品' v-bind="formItemLayout">
+            <a-select
+              style="width: 100%"
+              mode="tags"
+              placeholder="Please select"
+              :default-value="['a1', 'b2']"
+              @change="handleChange"
+            >
+              <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">
+                {{ (i + 9).toString(36) + i }}
+              </a-select-option>
+            </a-select>
+          </a-form-item>
+        </a-col>
+      </a-row>
+    </a-form>
+  </a-modal>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+function getBase64 (file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = error => reject(error)
+  })
+}
+const formItemLayout = {
+  labelCol: { span: 24 },
+  wrapperCol: { span: 24 }
+}
+export default {
+  name: 'costAdd',
+  props: {
+    costAddVisiable: {
+      default: false
+    }
+  },
+  computed: {
+    ...mapState({
+      currentUser: state => state.account.user
+    }),
+    show: {
+      get: function () {
+        return this.costAddVisiable
+      },
+      set: function () {
+      }
+    }
+  },
+  data () {
+    return {
+      formItemLayout,
+      form: this.$form.createForm(this),
+      loading: false,
+      fileList: [],
+      previewVisible: false,
+      previewImage: ''
+    }
+  },
+  methods: {
+    handleCancel () {
+      this.previewVisible = false
+    },
+    async handlePreview (file) {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj)
+      }
+      this.previewImage = file.url || file.preview
+      this.previewVisible = true
+    },
+    picHandleChange ({ fileList }) {
+      this.fileList = fileList
+    },
+    reset () {
+      this.loading = false
+      this.form.resetFields()
+    },
+    onClose () {
+      this.reset()
+      this.$emit('close')
+    },
+    handleSubmit () {
+      // 获取图片List
+      let images = []
+      this.fileList.forEach(image => {
+        if (image.response !== undefined) {
+          images.push(image.response)
+        } else {
+          images.push(image.name)
+        }
+      })
+      this.form.validateFields((err, values) => {
+        values.images = images.length > 0 ? images.join(',') : null
+        if (!err) {
+          values.publisher = this.currentUser.userId
+          this.loading = true
+          this.$post('/cos/cost-info', {
+            ...values
+          }).then((r) => {
+            this.reset()
+            this.$emit('success')
+          }).catch(() => {
+            this.loading = false
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
